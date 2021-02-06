1ff9b95ae01d1b782b3f105c64a5cf135bbafcfe
naiden nikolow <naiden452@gmail.com>
2021-02-06 14:22:57 +0200

    Colourful

--- UdemyGL/main.cpp
+++ UdemyGL/main.cpp
@@ -33,23 +33,27 @@ static const char* vShader = "					  \n\
 #version 330 								      \n\
 												  \n\
 layout (location = 0) in vec3 pos;				  \n\
+out vec4 gCol;								\n\
 												  \n\
-uniform mat4 model;							  \n\
+uniform mat4 model;							      \n\
 												  \n\
 void main()										  \n\
 {												  \n\
 	gl_Position = model * vec4(pos, 1.0); \n\
+	gCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);										\n\
 }";
 //Fragment Shader
 
 static const char* fShader = "								  \n\
 #version 330 												  \n\
+															\n\
+in vec4 gCol;															\n\
 															  \n\
 out vec4 colour;											  \n\
 															  \n\
 void main()													  \n\
 {															  \n\
-	colour = vec4(1.0, 0.0, 0.0, 1.0); \n\
+	colour = gCol;				//vec4(1.0, 0.0, 0.0, 1.0); \n\
 }";
 
 void CreateTriangle()
@@ -236,9 +240,9 @@ int main()
 
 		glm::mat4 model(1.0f);
 
-		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-		model = glm::translate(model, glm::vec3(basis, basis, 0.0f));
-		model = glm::scale(model, glm::vec3(curSize, curSize, 0.0f));
+		//model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+		//model = glm::translate(model, glm::vec3(basis, basis, 0.0f));
+		model = glm::scale(model, glm::vec3(0.8, 0.8, 0.0f));
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model)); 
 		// Why we have to use projection matrix?
