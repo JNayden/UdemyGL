af0986d3b80dcb5ff7171e9d6e11b31d1a15ff2f
naiden nikolow <naiden452@gmail.com>
2021-03-01 10:18:43 +0200

    test

--- UdemyGL/Light.h
+++ UdemyGL/Light.h
@@ -6,11 +6,17 @@ class Light
 {
 public:
 	Light();
-	Light(float Red, float Green, float Blue, float aIntensity);
-	void UseLight(float ambientIntensityLocation, float ambientColourLocation);
+	Light(float Red, float Green, float Blue, float aIntensity,
+		  float xDir, float yDir, float zDir, float dIntensity);
+	void UseLight(float ambientIntensityLocation, float ambientColourLocation,
+				  float diffuseIntensityLocation, float directionLocation);
+
 	~Light();
 private:
 	float ambientIntensity;
 	glm::vec3 colour;
+
+	float diffuseIntensity;
+	glm::vec3 direction;
 };
 
